6569d0bf66b81a0e7874d9177dd07e00aef56730
DayMax86 <daymax96@gmail.com>
2023-11-29 15:40:04 +0000

    Dictionary of images

---
+++ src/AssetLibrary.js
@@ -0,0 +1,4 @@
+export var images = {
+    ORACLE_DEFAULT: "https://firebasestorage.googleapis.com/v0/b/localise-aquamarine.appspot.com/o/oracle.png?alt=media&token=1d069c37-f88f-4f7e-8fcd-712798f3ae1a",
+    
+}
